d6ee977b95bc14e9414bb986745fd6f7dc5fafd3
wanjirugachunji <wanjirugachunji@gmail.com>
2022-06-28 18:19:50 +0300

    commit tree da0b9f492b8dd07987118b99a0194aef75fcf7f9

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+const submitData = (name, email) => {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({ name, email }),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data.id);
+            document.body.innerHTML = data.id;
+        })
+        .catch((error) => {
+            console.log(error.message);
+            document.body.innerHTML = error.message;
+        });
+};
